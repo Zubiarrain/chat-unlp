efc00c5dcbf698045a62e42953cde485261e82ea
Zubiarrain <zubiarrainnahuel@gmail.com>
2024-06-27 13:38:31 -0300

    fix: tipado

--- src/utils/saveApiKey.ts
+++ src/utils/saveApiKey.ts
@@ -6,7 +6,7 @@ import CryptoJS from 'crypto-js';
 export const saveApiKey = (apiKey:string) => {
     const cookiesApi = cookies()
     try {
-        const secretKey = process.env.COOKIE_SECRET_KEY
+        const secretKey = process.env.COOKIE_SECRET_KEY || ""
         const encryptedApiKey = CryptoJS.AES.encrypt(apiKey, secretKey).toString();
         cookiesApi.set('apiKey', encryptedApiKey, {
         path: "/",
